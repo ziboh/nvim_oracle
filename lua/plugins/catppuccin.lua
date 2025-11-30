afa520e735ce3465e8ca5da9fb14ac402825a2c3
zibo <sharezhou@outlook.com>
2025-11-30 20:24:32 +0800

    refactor: simplify catppuccin config
    - Remove integrations block
    - Remove telescope highlight overrides
    - Change NormalFloat background to transparent

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,38 +5,14 @@ vim.pack.add({
 require("catppuccin").setup({
 	transparent_background = true,
 	term_colors = true,
-	integrations = {
-		aerial = true,
-		diffview = true,
-		mini = {
-			enabled = true,
-			indentscope_color = "sky",
-		},
-		noice = true,
-		-- overseer = true,
-		telescope = {
-			enabled = true,
-			-- style = "nvchad",
-		},
-		treesitter = true,
-		notify = true,
-		gitsigns = true,
-		flash = true,
-		blink_cmp = true,
-		mason = true,
-		snacks = true,
-	},
 	highlight_overrides = {
 		mocha = function(mocha)
 			return {
 				CursorLineNr = { fg = mocha.yellow },
-				TelescopeSelection = { bg = mocha.surface0 },
-				TelescopeSelectionCaret = { fg = mocha.yellow, bg = mocha.surface0 },
-				TelescopePromptPrefix = { fg = mocha.yellow },
 				FlashCurrent = { bg = mocha.peach, fg = mocha.base },
 				FlashMatch = { bg = mocha.red, fg = mocha.base },
 				FlashLabel = { bg = mocha.teal, fg = mocha.base },
-				NormalFloat = { bg = mocha.base },
+				NormalFloat = { bg = "NONE" },
 				FloatBorder = { bg = mocha.base },
 				FloatTitle = { bg = mocha.base },
 				RenderMarkdownCode = { bg = mocha.crust },
