b4bc6fb0e524513d9386570bffe9e949793241d7
zibo <sharezhou@outlook.com>
2025-12-03 22:15:29 +0800

    style: update Catppuccin theme configuration

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,25 +1,26 @@
 vim.pack.add({
-    { src = "https://github.com/catppuccin/nvim" },
+	{ src = "https://github.com/catppuccin/nvim" },
 })
 
 require("catppuccin").setup({
-    transparent_background = true,
-    term_colors = true,
-    highlight_overrides = {
-        mocha = function(mocha)
-            return {
-                CursorLineNr = { fg = mocha.yellow },
-                NormalFloat = { bg = "NONE" },
-                FloatBorder = { bg = "NONE" },
-                FloatTitle = { bg = "NONE" },
-                RenderMarkdownCode = { bg = mocha.crust },
-                SnacksPickerBorder = { bg = "NONE" },
-                Pmenu = { bg = mocha.base },
-            }
-        end,
-    },
+	flavour = "macchiato", -- latte, frappe, macchiato, mocha ,auto
+	transparent_background = true,
+	term_colors = true,
+	highlight_overrides = {
+		mocha = function(mocha)
+			return {
+				CursorLineNr = { fg = mocha.yellow },
+				RenderMarkdownCode = { bg = mocha.crust },
+				Pmenu = { bg = mocha.base },
+			}
+		end,
+	},
 })
 vim.cmd("colorscheme catppuccin")
 vim.api.nvim_set_hl(0, "statusline", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "Comment", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatTitle", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "SnacksPickerBorder", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "YankyYanked", { fg = "#161b22", bg = "#ff8f62" })
